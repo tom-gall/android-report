21343fc8c7a346770196100bc4d05b4637e5b01a
Yongqin Liu <yongqin.liu@linaro.org>
2018-01-19 21:57:17 +0800

    add footer and style for cts focused tests
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

---
+++ report/templatetags/startswith.py
@@ -0,0 +1,10 @@
+## https://docs.djangoproject.com/en/2.0/howto/custom-template-tags/
+from django import template
+
+register = template.Library()
+
+@register.filter('startswith')
+def startswith(text, starts):
+    if isinstance(text, basestring):
+        return text.startswith(starts)
+    return False
