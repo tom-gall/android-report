fd302b7fb35574c3c6298f670d87c2de1583b6d4
Yongqin Liu <yongqin.liu@linaro.org>
2018-11-28 20:00:38 +0800

    lcr/settings.py: upload change for FILES_DIR
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- lcr/settings.py
+++ lcr/settings.py
@@ -21,6 +21,9 @@ bind_passwd=""
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Directoy used to store cts vts result files
+FILES_DIR = os.path.join(BASE_DIR, "files/cts-vts")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
