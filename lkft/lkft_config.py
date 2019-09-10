424228f7cf007cfcc610531894a5c68486219680
Yongqin Liu <yongqin.liu@linaro.org>
2019-09-10 19:23:43 +0800

    lkft/lkft_config.py: add support for new qgsi projects
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- lkft/lkft_config.py
+++ lkft/lkft_config.py
@@ -13,6 +13,7 @@ citrigger_lkft = {
         'mainline-9.0-x15-auto': 'lkft-android-9.0-mainline-x15',
         },
 
+    # configs for 4.19 kernels
     'trigger-lkft-ti-4.19': {
         '4.19-9.0-x15': 'lkft-x15-android-9.0-4.19',
         '4.19-9.0-x15-auto': 'lkft-x15-android-9.0-4.19',
@@ -24,15 +25,21 @@ citrigger_lkft = {
         '4.19-9.0-hikey-auto': 'lkft-hikey-android-9.0-4.19',
         '4.19-9.0-hikey960': 'lkft-hikey-android-9.0-4.19',
         '4.19-9.0-hikey960-auto': 'lkft-hikey-android-9.0-4.19',
+        '4.19-10.0-gsi-hikey': 'lkft-hikey-android-10.0-gsi-4.19',
+        '4.19-10.0-gsi-hikey960': 'lkft-hikey-android-10.0-gsi-4.19',
         },
-
     'trigger-lkft-hikey-aosp-4.19-r': {
         '4.19-master-hikey': 'lkft-hikey-aosp-master-4.19',
         '4.19-master-hikey960': 'lkft-hikey-aosp-master-4.19',
         },
 
+    # configs for 4.14 kernels
     'trigger-lkft-x15-4.14': {
         '4.14-8.1-x15': 'lkft-x15-android-8.1-4.14',
+        },
+    'trigger-lkft-hikey-4.14-q': {
+        '4.14-10.0-gsi-hikey': 'lkft-hikey-10.0-4.14-q',
+        '4.14-10.0-gsi-hikey960': 'lkft-hikey-10.0-4.14-q',
         },
     'trigger-lkft-hikey-4.14-premerge-ci': {
         '4.14-9.0-hikey': 'lkft-hikey-aosp-4.14-premerge-ci',
@@ -42,6 +49,11 @@ citrigger_lkft = {
         '4.14-8.1-hikey': 'lkft-hikey-android-8.1-4.14',
         },
 
+    # configs for 4.9 kernels
+    'trigger-lkft-hikey-4.9-q': {
+        '4.9-10.0-gsi-hikey': 'lkft-hikey-10.0-4.9-q',
+        '4.9-10.0-gsi-hikey960': 'lkft-hikey-10.0-4.9-q',
+        },
     'trigger-lkft-hikey-4.9-premerge-ci': {
         '4.9-9.0-hikey': 'lkft-hikey-aosp-4.9-premerge-ci',
         '4.9-9.0-hikey960': 'lkft-hikey-aosp-4.9-premerge-ci',
@@ -50,6 +62,7 @@ citrigger_lkft = {
         '4.9-8.1-hikey': 'lkft-hikey-android-8.1-4.9',
         },
 
+    # configs for 4.4 kernels
     'trigger-lkft-hikey-4.4-premerge-ci': {
         '4.4-lts-9.0-hikey': 'lkft-hikey-aosp-4.4-premerge-ci',
         },
