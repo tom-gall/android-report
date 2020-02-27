746a728a3237c92236d5a39ac46c4aaa22d75a5c
Yongqin Liu <yongqin.liu@linaro.org>
2020-02-28 00:26:32 +0800

    lcr/settings.py: set STATIC_ROOT
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- lcr/settings.py
+++ lcr/settings.py
@@ -197,10 +197,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# python  manage.py collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
 
 LOGGING = {
     'version': 1,
