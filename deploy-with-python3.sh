24d45f8d3904f633aa62daf2dd8fa4cb09ee1159
Yongqin Liu <yongqin.liu@linaro.org>
2020-04-08 09:47:31 +0800

    deploy-with-python3.sh: update to use Django==1.11.17
    to work with python3.7 for the problem reported here:
    
        SyntaxError: Generator expression must be parenthesized
        https://stackoverflow.com/questions/51265858/syntaxerror-generator-expression-must-be-parenthesized
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    Change-Id: Id6c43a261a57dcfee007d80b9dbdee97141824f1

--- deploy-with-python3.sh
+++ deploy-with-python3.sh
@@ -48,17 +48,17 @@ source ${virenv_dir}/bin/activate
 #$ rm -r /path/to/ENV
 
 #https://docs.djangoproject.com/en/1.11/topics/install/#installing-official-release
-pip install Django==1.11.8 pyaml django-crispy-forms python-ldap django-auth-ldap requests reportlab psycopg2
+pip install Django==1.11.17 pyaml django-crispy-forms python-ldap django-auth-ldap requests reportlab psycopg2
 #pip install Django==1.11.8
 #pip install Django==3.0.8
-pip install pyaml
-pip install django-crispy-forms==1.8.1
-pip install python-ldap # will install the 3.0 version
+#pip install pyaml
+#pip install django-crispy-forms==1.8.1
+#pip install python-ldap # will install the 3.0 version
 # https://django-auth-ldap.readthedocs.io/en/latest/install.html
-pip install django-auth-ldap # needs python-ldap >= 3.0
+#pip install django-auth-ldap # needs python-ldap >= 3.0
 #pip install bugzilla
-pip install requests
-pip install reportlab
+#pip install requests
+#pip install reportlab
 ## pip install Pillow
 ## pip install rst2pdf
 
