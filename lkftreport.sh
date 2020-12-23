a15e55f1dc9a837ead89f7d723d7ce4849ba1331
Yongqin Liu <yongqin.liu@linaro.org>
2020-12-23 19:55:39 +0800

    lkftreport.sh: accept parameters from command line
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    Change-Id: I08580c08273975a15a060922598ea15ae0298713

--- lkftreport.sh
+++ lkftreport.sh
@@ -2,4 +2,4 @@
 
 dir_parent=$(cd $(dirname $0); pwd)
 
-${dir_parent}/../workspace-python3/bin/python ${dir_parent}/../lcr-report/manage.py lkftreport
+${dir_parent}/../workspace-python3/bin/python ${dir_parent}/../lcr-report/manage.py lkftreport "$@"
